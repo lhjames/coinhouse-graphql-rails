3d98cab79e4c4a5cc64fc828a80c280a27b3fc78
Lucille James <lucille.james00@gmail.com>
2021-03-17 11:22:41 +0100

    implemented the beginning of the search code

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,6 +2,10 @@ module Types
   class QueryType < BaseObject
     field :all_workshops, [WorkshopType], null: false
     field :all_meetings, [MeetingType], null: false
+    # Your client should always be able to expect to work with a list
+    field :search_workshops, [WorkshopType], null: false do
+      argument :location, String, required: false
+    end 
 
     def all_workshops
       Workshop.all
@@ -10,5 +14,11 @@ module Types
     def all_meetings
       Meeting.all
     end
+
+    def search_workshops(**args)
+      byebug
+      Workshop.all
+    end
+    #Take that, figure out the query, how to make sure it works, WRITE TESTS, make the test pass (use byebug)
   end
 end
